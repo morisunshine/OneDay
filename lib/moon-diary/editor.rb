21bec42277b0ccb54c5eca4ca0fcfe43b284c715
morisunshine <sheldon.zen@gmail.com>
2014-06-26 22:29:16 +0800

    feat:add the bin and lib

---
+++ lib/moon-diary/editor.rb
@@ -0,0 +1,14 @@
+require 'fileutils'
+require 'tempfile'
+
+module DiaryEditor
+	class Diary
+		def initialize(*args, opts)
+
+		end
+
+		def run
+
+		end
+	end
+end
---
+++ lib/moon-diary/version.rb
@@ -0,0 +1,3 @@
+module MoonDiary
+	VERSION = 0.0.0
+end
